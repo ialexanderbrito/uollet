6bbe8e00a3fd97feda91bf28828e9f05d2ad41f3
ialexanderbrito <xanderzinho26@gmail.com>
2023-03-18 21:14:54 -0300

    feat: add config tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx', './index.html'],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#5636D3',
+        secondary: '#FF872C',
+        success: '#12A454',
+        danger: '#E83F5B',
+        title: '#363F5F',
+        text: '#969CB2',
+        background: '#F0F2F5',
+        backgroundCard: '#FFFFFF',
+        primaryDark: '#3d24a2',
+        secondaryDark: '#b24d00',
+        titleDark: '#a5b7cc',
+        textDark: '#babab7',
+        backgroundDark: '#1f2223',
+        backgroundCardDark: '#181a1b',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/line-clamp'), require('tailwind-scrollbar'), require('tailwind-scrollbar-hide')],
+};
