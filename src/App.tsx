a898fc5b65763c8e631cf309bfad2bbf9f46b67e
ialexanderbrito <xanderzinho26@gmail.com>
2023-03-18 22:29:05 -0300

    feat: add import contexts global

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { MainRoutes } from 'routes';
 
+import { AuthProvider } from 'contexts/Auth';
 import { ThemeProvider } from 'contexts/Theme';
 import { ToastProvider } from 'contexts/Toast';
 
 export function App() {
   return (
-    <ToastProvider>
+    <BrowserRouter>
       <ThemeProvider>
-        <BrowserRouter>
-          <MainRoutes />
-        </BrowserRouter>
+        <AuthProvider>
+          <ToastProvider>
+            <MainRoutes />
+          </ToastProvider>
+        </AuthProvider>
       </ThemeProvider>
-    </ToastProvider>
+    </BrowserRouter>
   );
 }
