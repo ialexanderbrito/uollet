6345ad8106aa7122e1f3b192d492426da2f90ed0
ialexanderbrito <xanderzinho26@gmail.com>
2023-03-27 16:13:54 -0300

    feat: add update transaction by id

--- src/hooks/useRegister.tsx
+++ src/hooks/useRegister.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
@@ -10,11 +10,24 @@ import { useToast } from 'contexts/Toast';
 import { supabase } from 'services/supabase';
 
 export function useRegister() {
+  const { id } = useParams();
   const navigate = useNavigate();
   const { toast } = useToast();
   const { storageUser } = useAuth();
 
-  const [valor, setValor] = useState(0);
+  function verifyIfIsNumber(value: string) {
+    const regex = /^[0-9]+$/;
+
+    return regex.test(value);
+  }
+
+  function convertVirgulaToPonto(value: string) {
+    const newValue = value.replace(',', '.');
+
+    const number = Number(newValue);
+
+    return number;
+  }
 
   const schema = Yup.object({
     title: Yup.string().required('Campo obrigatório'),
@@ -36,36 +49,100 @@ export function useRegister() {
     },
     validationSchema: schema,
     onSubmit: async (values) => {
-      if (!valor) {
+      if (!values.value) {
         toast.error('Adicione um valor!', { id: 'error' });
         return;
       }
 
       try {
-        const { error } = await supabase.from('finances_db').insert([
-          {
-            title: values.title,
-            value: valor,
-            category: values.category,
-            date: values.date,
-            type: values.type,
-            user_id: storageUser?.id,
-          },
-        ]);
+        if (!id) {
+          const { error } = await supabase.from('finances_db').insert([
+            {
+              title: values.title,
+              value: verifyIfIsNumber(values.value)
+                ? values.value
+                : convertVirgulaToPonto(values.value),
+              category: values.category,
+              date: values.date,
+              type: values.type,
+              user_id: storageUser?.id,
+            },
+          ]);
 
-        if (error) {
-          toast.error('Erro ao cadastrar!', { id: 'error' });
-          return;
+          if (error) {
+            toast.error('Erro ao cadastrar!', { id: 'error' });
+            return;
+          }
+
+          toast.success('Cadastrado com sucesso!', { id: 'success' });
+
+          navigate('/');
         }
 
-        toast.success('Cadastrado com sucesso!', { id: 'success' });
+        if (id) {
+          const { error } = await supabase
+            .from('finances_db')
+            .update({
+              id,
+              title: values.title,
+              value: verifyIfIsNumber(values.value)
+                ? values.value
+                : convertVirgulaToPonto(values.value),
+              category: values.category,
+              date: values.date,
+              type: values.type,
+              user_id: storageUser?.id,
+            })
+            .eq('id', id)
+            .eq('user_id', storageUser?.id);
 
-        navigate('/');
+          if (error) {
+            toast.error('Erro ao atualizar!', { id: 'error' });
+            return;
+          }
+
+          toast.success('Atualizado com sucesso!', { id: 'success' });
+
+          navigate('/');
+        }
       } catch (error) {
         toast.error('Erro ao cadastrar!', { id: 'error' });
       }
     },
   });
 
-  return { formik, valor, setValor };
+  async function updateFinance() {
+    if (!id) return;
+
+    try {
+      const { data, error } = await supabase
+        .from('finances_db')
+        .select('*')
+        .eq('id', id)
+        .eq('user_id', storageUser?.id);
+
+      if (error) {
+        toast.error('Erro ao buscar dados!', { id: 'error' });
+        return;
+      }
+
+      if (data) {
+        formik.setFieldValue('title', data[0].title);
+        formik.setFieldValue('category', data[0].category);
+        formik.setFieldValue('date', data[0].date);
+        formik.setFieldValue('type', data[0].type);
+        formik.setFieldValue('value', data[0].value);
+      }
+    } catch (error) {
+      toast.error('Erro ao buscar dados!', { id: 'error' });
+    }
+  }
+
+  useEffect(() => {
+    if (id) {
+      updateFinance();
+    }
+  }, [id]);
+
+  return { formik };
 }
