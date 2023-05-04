1c4d62c2c3cfbddbc702384ad74e24683e45d5ae
ialexanderbrito <xanderzinho26@gmail.com>
2023-05-04 20:49:35 -0300

    feat: add new page my account

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,16 +1,24 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Eye, EyeSlash } from '@phosphor-icons/react';
+import {
+  CaretLeft,
+  Info,
+  Key,
+  LockKey,
+  Question,
+  SignOut,
+  Sun,
+  UserMinus,
+} from '@phosphor-icons/react';
 import defaultAvatar from 'assets/default_user_avatar.png';
 
 import { BottomNavigator } from 'components/BottomNavigator';
 import { Loading } from 'components/Loading';
 import { MyDialog } from 'components/Modal';
-
-import { formatCurrency } from 'utils/formatCurrency';
+import { Submenu } from 'components/Submenu';
 
 import { useAuth } from 'contexts/Auth';
+import { useToast } from 'contexts/Toast';
 
 import { useProfile } from 'hooks/useProfile';
 import { useTransactions } from 'hooks/useTransactions';
@@ -28,20 +36,12 @@ export interface FinancesProps {
 
 export function Profile() {
   const navigate = useNavigate();
+  const { toast } = useToast();
   const { user, logOut } = useAuth();
   const { deleteUser } = useProfile();
 
-  const {
-    loading,
-    totalIncome,
-    allTotal,
-    totalOutcome,
-    handleCloseModal,
-    openModal,
-    handleOpenModal,
-  } = useTransactions();
-
-  const [visible, setVisible] = useState(false);
+  const { loading, handleCloseModal, openModal, handleOpenModal } =
+    useTransactions();
 
   return (
     <>
@@ -50,89 +50,108 @@ export function Profile() {
       ) : (
         <div className="flex w-full flex-col items-center justify-center bg-background dark:bg-backgroundDark">
           <div className="flex h-24 w-full flex-row bg-primary dark:bg-primaryDark">
-            <div className="flex w-full items-center justify-center gap-4">
-              <p className="text-lg font-normal text-white">
-                Olá,{' '}
-                <span className="font-medium">{user?.user_metadata.name}</span>
-              </p>
-              <div className="flex flex-row items-center justify-center gap-2">
-                <span className="font-medium">
-                  {visible ? (
-                    <Eye
-                      size={20}
-                      className="cursor-pointer text-secondary"
-                      onClick={() => setVisible(!visible)}
-                    />
-                  ) : (
-                    <EyeSlash
-                      size={20}
-                      className="cursor-pointer text-secondary"
-                      onClick={() => setVisible(!visible)}
-                    />
-                  )}
-                </span>
-              </div>
+            <div className="flex w-1/4 items-center justify-center">
+              <CaretLeft
+                size={20}
+                weight="light"
+                className="cursor-pointer text-white"
+                onClick={() => navigate(-1)}
+              />
+            </div>
+            <div className="flex w-2/4 items-center justify-center">
+              <p className="text-lg font-normal text-white">Minha conta</p>
             </div>
           </div>
 
           <div className="flex min-h-[85vh] w-full flex-col gap-4 p-4">
-            <div className="flex flex-row items-center justify-center gap-4">
+            <div className="flex h-16 flex-row items-center justify-start gap-4 rounded-lg bg-backgroundCard dark:bg-backgroundCardDark">
               <img
                 src={user?.user_metadata.avatar_url || defaultAvatar}
                 alt={user?.user_metadata.name}
-                className="h-20 w-20 rounded-lg"
+                className="ml-3 h-10 w-10 rounded-lg"
               />
+
+              <div>
+                <span className="font-medium text-title dark:text-titleDark">
+                  {user?.user_metadata.full_name}
+                </span>
+                <p className="text-xs font-normal text-text dark:text-textDark">
+                  Perfil
+                </p>
+              </div>
             </div>
 
-            <div className="flex flex-col items-start justify-center gap-4 text-title dark:text-titleDark">
-              <span className="flex flex-row items-center justify-center gap-2">
-                Email: <span className="font-medium">{user?.email}</span>
-              </span>
-
-              <span className="flex flex-row items-center justify-center gap-2">
-                Entradas do mês:
-                <span className="font-medium">
-                  {visible ? formatCurrency(totalIncome) : '*****'}
-                </span>
-              </span>
-
-              <span className="flex flex-row items-center justify-center gap-2">
-                Saídas do mês:
-                <span className="font-medium">
-                  {visible ? formatCurrency(totalOutcome) : '*****'}
-                </span>
-              </span>
-
-              <span className="flex flex-row items-center justify-center gap-2">
-                Total:
-                <span className="font-medium">
-                  {visible ? formatCurrency(allTotal) : '*****'}
-                </span>
-              </span>
-            </div>
-
-            <div className="flex flex-col items-center justify-end gap-4">
-              <button
+            <div className="mt-4 flex flex-col items-start justify-center gap-4 text-title dark:text-titleDark">
+              <Submenu
+                icon={<LockKey size={20} weight="light" />}
+                title="Segurança"
                 onClick={() => navigate('/otp')}
-                type="button"
-                className="h-14 w-full rounded-lg bg-secondary p-4 text-white dark:bg-secondaryDark"
-              >
-                Adicionar senha de acesso
-              </button>
-              <button
-                onClick={() => logOut()}
-                type="button"
-                className="h-14 w-full rounded-lg bg-secondary p-4 text-white dark:bg-secondaryDark"
-              >
-                Sair
-              </button>
-              <button
+                arrow
+                divider
+              />
+
+              <Submenu
+                icon={<Key size={20} weight="light" />}
+                title="Senha"
+                onClick={() => {
+                  toast.error('Em breve você poderá alterar sua senha!', {
+                    id: 'toast',
+                  });
+                }}
+                arrow
+                divider
+              />
+
+              <Submenu
+                icon={<Info size={20} weight="light" />}
+                title="Ajuda"
+                onClick={() => {
+                  toast.error(
+                    'Em breve você poderá entrar em contato com o suporte!',
+                    {
+                      id: 'toast',
+                    },
+                  );
+                }}
+                arrow
+                divider
+              />
+
+              <Submenu
+                icon={<Question size={20} weight="light" />}
+                title="Sobre"
+                onClick={() => {
+                  toast.error(
+                    'Em breve você poderá ver informações sobre o app!',
+                    {
+                      id: 'toast',
+                    },
+                  );
+                }}
+                arrow
+                divider
+              />
+
+              <Submenu
+                icon={<Sun size={20} weight="light" />}
+                title="Tema do app"
+                divider
+                switchTheme
+              />
+
+              <Submenu
+                icon={<UserMinus size={20} weight="light" />}
+                title="Apagar a conta"
                 onClick={() => handleOpenModal()}
-                type="button"
-                className="h-14 w-full rounded-lg bg-danger p-4 text-white dark:bg-danger"
-              >
-                Deletar Conta
-              </button>
+                arrow
+                divider
+              />
+
+              <Submenu
+                icon={<SignOut size={20} weight="light" />}
+                title="Sair"
+                onClick={() => logOut()}
+              />
             </div>
 
             <MyDialog
@@ -141,6 +160,7 @@ export function Profile() {
               title="Deletar Conta"
               description="Tem certeza que deseja deletar sua conta? Essa ação não poderá ser desfeita e todos os seus dados serão perdidos."
               deleteTransaction={() => deleteUser(user?.id || '')}
+              terms
             />
           </div>
 
