4df1a94caaa6e64c6d02255433598a32c45b450c
ialexanderbrito <xanderzinho26@gmail.com>
2023-05-04 20:46:00 -0300

    feat: add color toast and position center

--- src/contexts/Toast.tsx
+++ src/contexts/Toast.tsx
@@ -1,20 +1,37 @@
 import { createContext, useContext } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 
+import { useTheme } from './Theme';
+
 type IToastProps = {
   toast: any;
 };
 
 const ToastContext = createContext<IToastProps>({} as any);
 
-export const ToastProvider = ({ children }: any) => (
-  <ToastContext.Provider value={{ toast }}>
-    {children}
-    <div className="toast-wrapper">
-      <Toaster position="top-right" reverseOrder={false} />
-    </div>
-  </ToastContext.Provider>
-);
+export const ToastProvider = ({ children }: any) => {
+  const { theme } = useTheme();
+
+  return (
+    <ToastContext.Provider value={{ toast }}>
+      {children}
+      <div className="toast-wrapper">
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          toastOptions={{
+            className: '',
+            style: {
+              background: theme === 'dark' ? '#333' : '#fff',
+              color: theme === 'dark' ? '#fff' : '#333',
+            },
+            duration: 3000,
+          }}
+        />
+      </div>
+    </ToastContext.Provider>
+  );
+};
 
 export function useToast() {
   const context = useContext(ToastContext);
