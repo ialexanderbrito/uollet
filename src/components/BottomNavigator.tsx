3c05af6744144c3fe6d410741e1bac672ca24a3b
ialexanderbrito <xanderzinho26@gmail.com>
2023-06-03 14:20:49 -0300

    feat: add new navigator floating and new icons

--- src/components/BottomNavigator.tsx
+++ src/components/BottomNavigator.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-import { CurrencyDollar, ListDashes, ChartPie } from '@phosphor-icons/react';
+import { CurrencyDollar, ChartPie, House } from '@phosphor-icons/react';
 
 export function BottomNavigator() {
   function isActive(path: string) {
@@ -10,50 +10,62 @@ export function BottomNavigator() {
   return (
     <section
       id="bottom-navigation"
-      className="fixed inset-x-0 bottom-0 z-10 block bg-white shadow dark:bg-backgroundCardDark"
+      className="fixed bottom-5 z-10 w-72 rounded-2xl border border-secondary/40 bg-white shadow dark:border-secondaryDark/40 dark:bg-backgroundDark sm:w-96"
     >
       <div
         id="tabs"
-        className="border-t-[1px]-background flex h-16 justify-between border-solid text-title dark:text-titleDark"
+        className="border-t-[1px]-background flex h-16 items-center justify-between border-solid text-title dark:text-titleDark"
       >
         <Link
           to="/"
           className={`flex w-full items-center justify-center gap-2 pb-1 pt-2 hover:text-secondary focus:text-secondary ${
-            isActive('/') && 'text-secondary'
+            isActive('/') &&
+            'h-16 rounded-2xl bg-secondary/10 text-secondary dark:bg-secondaryDark/10'
           }`}
         >
-          <ListDashes
+          <House
             size={25}
             className="inline-block"
             color={isActive('/') ? '#ff872c' : '#363F5F'}
+            weight={isActive('/') ? 'fill' : 'light'}
           />
-          <span className="tab tab-home block text-sm">Listagem</span>
+          <span className="tab tab-explore hidden text-sm sm:block">
+            Início
+          </span>
         </Link>
         <Link
           to="/register"
-          className={`flex w-full items-center justify-center gap-2 pb-1 pt-2 hover:text-secondary focus:text-secondary ${
-            isActive('/register') && 'text-secondary'
+          className={`flex w-full items-center justify-center gap-2 pb-1 pt-2 hover:text-secondary focus:text-secondary  ${
+            isActive('/register') &&
+            'h-16 rounded-2xl bg-secondary/10 text-secondary dark:bg-secondaryDark/10'
           }`}
         >
           <CurrencyDollar
             size={25}
             className="inline-block"
             color={isActive('/register') ? '#ff872c' : '#363F5F'}
+            weight={isActive('/register') ? 'fill' : 'light'}
           />
-          <span className="tab tab-home block text-sm">Cadastrar</span>
+          <span className="tab tab-explore hidden text-sm sm:block">
+            Cadastrar
+          </span>
         </Link>
         <Link
           to="/resume"
-          className={`flex w-full items-center justify-center gap-2 pb-1 pt-2 hover:text-secondary focus:text-secondary ${
-            isActive('/resume') && 'text-secondary'
+          className={`flex w-full items-center justify-center gap-2 pb-1 pt-2 hover:text-secondary focus:text-secondary  ${
+            isActive('/resume') &&
+            'h-16 rounded-2xl bg-secondary/10 text-secondary dark:bg-secondaryDark/10'
           }`}
         >
           <ChartPie
             size={25}
             className="inline-block"
             color={isActive('/resume') ? '#ff872c' : '#363F5F'}
+            weight={isActive('/resume') ? 'fill' : 'light'}
           />
-          <span className="tab tab-explore block text-sm">Resumo</span>
+          <span className="tab tab-explore hidden text-sm sm:block">
+            Resumo
+          </span>
         </Link>
       </div>
     </section>
