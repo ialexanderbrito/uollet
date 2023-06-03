fd70e11774e5a0fbddf1869410693d3898ed4975
ialexanderbrito <xanderzinho26@gmail.com>
2023-06-03 14:19:38 -0300

    feat: add recurrency register

--- src/hooks/useRegister.tsx
+++ src/hooks/useRegister.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { useFormik } from 'formik';
@@ -14,6 +14,21 @@ export function useRegister() {
   const navigate = useNavigate();
   const { toast } = useToast();
   const { storageUser } = useAuth();
+
+  const [isRecurring, setIsRecurring] = useState(false);
+  const [openBottomSheet, setOpenBottomSheet] = useState(false);
+
+  function handleSwitch() {
+    setIsRecurring(!isRecurring);
+
+    if (!isRecurring) {
+      setOpenBottomSheet(!openBottomSheet);
+    }
+
+    if (isRecurring) {
+      formik.setFieldValue('recurrency', '');
+    }
+  }
 
   function verifyIfIsNumber(value: string) {
     const regex = /^[0-9]+$/;
@@ -51,6 +66,7 @@ export function useRegister() {
       date: '',
       type: '',
       user_id: '',
+      recurrency: '',
     },
     validationSchema: schema,
     onSubmit: async (values) => {
@@ -73,6 +89,29 @@ export function useRegister() {
               user_id: storageUser?.id,
             },
           ]);
+
+          if (values.recurrency) {
+            const { error: errorRecurrency } = await supabase
+              .from('finances_recurrency')
+              .insert([
+                {
+                  title: values.title,
+                  recurrency: values.recurrency,
+                  user_id: storageUser?.id,
+                  value: verifyIfIsNumber(values.value)
+                    ? values.value
+                    : convertVirgulaToPonto(values.value),
+                  category: values.category.name,
+                  date: values.date,
+                  type: values.type,
+                },
+              ]);
+
+            if (errorRecurrency) {
+              toast.error('Erro ao cadastrar!', { id: 'error' });
+              return;
+            }
+          }
 
           if (error) {
             toast.error('Erro ao cadastrar!', { id: 'error' });
@@ -101,6 +140,32 @@ export function useRegister() {
             .eq('id', id)
             .eq('user_id', storageUser?.id);
 
+          if (values.recurrency) {
+            const { error: errorRecurrency } = await supabase
+              .from('finances_recurrency')
+              .update([
+                {
+                  id,
+                  recurrency: values.recurrency,
+                  title: values.title,
+                  user_id: storageUser?.id,
+                  value: verifyIfIsNumber(values.value)
+                    ? values.value
+                    : convertVirgulaToPonto(values.value),
+                  category: values.category.name,
+                  date: values.date,
+                  type: values.type,
+                },
+              ])
+              .eq('id', id)
+              .eq('user_id', storageUser?.id);
+
+            if (errorRecurrency) {
+              toast.error('Erro ao atualizar!', { id: 'error' });
+              return;
+            }
+          }
+
           if (error) {
             toast.error('Erro ao atualizar!', { id: 'error' });
             return;
@@ -119,32 +184,44 @@ export function useRegister() {
   async function updateFinance() {
     if (!id) return;
 
-    try {
-      const { data, error } = await supabase
-        .from('finances_db')
-        .select('*')
-        .eq('id', id)
-        .eq('user_id', storageUser?.id);
+    const { data, error } = await supabase
+      .from('finances_db')
+      .select('*')
+      .eq('id', id)
+      .eq('user_id', storageUser?.id);
 
-      if (error) {
-        toast.error('Erro ao buscar dados!', { id: 'error' });
-        return;
-      }
+    if (!data) return;
 
-      if (data) {
-        const newCategory = {
-          name: data[0].category,
-          icon: '',
-        };
+    const { data: recurrencyData } = await supabase
+      .from('finances_recurrency')
+      .select('*')
+      .eq('title', data[0].title)
+      .eq('user_id', storageUser?.id);
 
-        formik.setFieldValue('title', data[0].title);
-        formik.setFieldValue('category', newCategory);
-        formik.setFieldValue('date', data[0].date);
-        formik.setFieldValue('type', data[0].type);
-        formik.setFieldValue('value', data[0].value);
-      }
-    } catch (error) {
+    if (!recurrencyData) return;
+
+    if (error) {
       toast.error('Erro ao buscar dados!', { id: 'error' });
+      return;
+    }
+
+    if (data && recurrencyData) {
+      const newCategory = {
+        name: data[0].category,
+        icon: '',
+      };
+
+      formik.setFieldValue('title', data[0].title);
+      formik.setFieldValue('category', newCategory);
+      formik.setFieldValue('date', data[0].date);
+      formik.setFieldValue('type', data[0].type);
+      formik.setFieldValue('value', data[0].value);
+
+      if (recurrencyData[0].recurrency) {
+        setIsRecurring(true);
+      }
+
+      formik.setFieldValue('recurrency', recurrencyData[0].recurrency);
     }
   }
 
@@ -154,5 +231,11 @@ export function useRegister() {
     }
   }, [id]);
 
-  return { formik };
+  return {
+    formik,
+    isRecurring,
+    handleSwitch,
+    openBottomSheet,
+    setOpenBottomSheet,
+  };
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,9 @@
 import CurrencyInput from 'react-currency-input-field';
 import { useNavigate } from 'react-router-dom';
+import { BottomSheet } from 'react-spring-bottom-sheet';
 
-import { CaretLeft } from '@phosphor-icons/react';
+import { RadioGroup, Switch } from '@headlessui/react';
+import { CaretLeft, CheckCircle } from '@phosphor-icons/react';
 import incomeIcon from 'assets/income.svg';
 import outcomeIcon from 'assets/outcome.svg';
 
@@ -10,7 +12,11 @@ import { BottomNavigator } from 'components/BottomNavigator';
 
 import { category } from 'utils/category';
 
+import { useToast } from 'contexts/Toast';
+
 import { useRegister } from 'hooks/useRegister';
+
+import 'react-spring-bottom-sheet/dist/style.css';
 
 export interface FinancesProps {
   id: number;
@@ -24,8 +30,21 @@ export interface FinancesProps {
 }
 
 export function Register() {
-  const { formik } = useRegister();
+  const { toast } = useToast();
+  const {
+    formik,
+    isRecurring,
+    handleSwitch,
+    openBottomSheet,
+    setOpenBottomSheet,
+  } = useRegister();
   const navigate = useNavigate();
+
+  function openToastAlert() {
+    toast.error('Em breve, você poderá cadastrar transações recorrentes!', {
+      id: 'recurring',
+    });
+  }
 
   return (
     <div className="flex w-full flex-col items-center bg-background dark:bg-backgroundDark">
@@ -44,7 +63,7 @@ export function Register() {
       </div>
 
       <form className="flex w-full flex-col" onSubmit={formik.handleSubmit}>
-        <div className="flex min-h-[85vh] w-full flex-col gap-4 p-4">
+        <div className="flex min-h-[90vh] w-full flex-col gap-4 p-4">
           <input
             type="text"
             className={`h-14 w-full rounded-lg bg-white p-4 text-title outline-none dark:bg-backgroundCardDark dark:text-titleDark ${
@@ -71,7 +90,6 @@ export function Register() {
                 : ''
             }`}
           />
-
           <div className="flex w-full flex-row justify-around gap-4 p-1">
             <div
               className={`flex h-16 w-full cursor-pointer flex-row items-center justify-evenly rounded-md border-[1.5px] border-solid border-text ${
@@ -118,7 +136,6 @@ export function Register() {
               <span className="text-xs text-red-500">{formik.errors.type}</span>
             ) : null}
           </div>
-
           <Autocomplete
             selected={formik.values.category}
             setSelected={(value) => {
@@ -131,7 +148,6 @@ export function Register() {
                 : ''
             }`}
           />
-
           <input
             type="date"
             className={`h-14 w-full rounded-lg bg-white p-4 text-title outline-none dark:bg-backgroundCardDark dark:text-titleDark ${
@@ -142,6 +158,174 @@ export function Register() {
             placeholder="DD/MM/AAAA"
             {...formik.getFieldProps('date')}
           />
+
+          <div
+            className="switch flex w-full flex-row items-center justify-end"
+            onClick={() => openToastAlert()}
+          >
+            <Switch
+              disabled
+              checked={isRecurring}
+              onChange={() => handleSwitch()}
+              className={`${
+                isRecurring ? 'bg-success' : 'bg-danger'
+              } switch relative inline-flex h-6 w-11 items-center rounded-full`}
+            >
+              <span
+                className={`${
+                  isRecurring ? 'translate-x-6' : 'translate-x-1'
+                } switch inline-block h-4 w-4 transform rounded-full bg-white`}
+              />
+            </Switch>
+
+            <span
+              className="switch ml-2 cursor-pointer text-sm text-text"
+              onClick={() => {}}
+            >
+              Receita recorrente
+            </span>
+          </div>
+
+          <BottomSheet
+            open={openBottomSheet}
+            defaultSnap={0}
+            onDismiss={() => setOpenBottomSheet(false)}
+            snapPoints={({ maxHeight }) => [maxHeight - 400, maxHeight - 400]}
+            className="bg-backgroundCard dark:bg-backgroundCardDark"
+          >
+            <div className="flex flex-col items-center justify-center gap-4 bg-backgroundCard dark:bg-backgroundCardDark">
+              <p className="ml-4 text-lg font-normal text-text">
+                A receita se repete a cada quanto tempo?
+              </p>
+              <div className="flex w-full flex-row items-center justify-evenly gap-4">
+                <RadioGroup
+                  onChange={(value) => {
+                    formik.setFieldValue('recurrency', value);
+                    setOpenBottomSheet(false);
+                  }}
+                  className={`mb-1 mt-1 flex w-full flex-col items-center justify-center gap-4`}
+                  value={formik.values.recurrency}
+                >
+                  <RadioGroup.Option
+                    value="day"
+                    className={({ active, checked }) => `${
+                      active
+                        ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-secondary'
+                        : ''
+                    } ${
+                      checked
+                        ? 'bg-secondary bg-opacity-75 text-white'
+                        : 'bg-white'
+                    }
+                    relative flex w-11/12 cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none
+                  `}
+                  >
+                    {({ active, checked }) => (
+                      <>
+                        <div className="flex w-full items-center justify-between">
+                          <div className="flex items-center">
+                            <div className="text-sm">
+                              <RadioGroup.Label
+                                as="p"
+                                className={`font-medium  ${
+                                  checked ? 'text-white' : 'text-gray-900'
+                                }`}
+                              >
+                                Diariamente
+                              </RadioGroup.Label>
+                            </div>
+                          </div>
+                          {checked && (
+                            <div className="flex-shrink-0 text-white">
+                              <CheckCircle className="h-6 w-6" />
+                            </div>
+                          )}
+                        </div>
+                      </>
+                    )}
+                  </RadioGroup.Option>
+
+                  <RadioGroup.Option
+                    value="week"
+                    className={({ active, checked }) => `${
+                      active
+                        ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-secondary'
+                        : ''
+                    } ${
+                      checked
+                        ? 'bg-secondary bg-opacity-75 text-white'
+                        : 'bg-white'
+                    }
+                    relative flex w-11/12 cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none
+                  `}
+                  >
+                    {({ active, checked }) => (
+                      <>
+                        <div className="flex w-full items-center justify-between">
+                          <div className="flex items-center">
+                            <div className="text-sm">
+                              <RadioGroup.Label
+                                as="p"
+                                className={`font-medium  ${
+                                  checked ? 'text-white' : 'text-gray-900'
+                                }`}
+                              >
+                                Semanalmente
+                              </RadioGroup.Label>
+                            </div>
+                          </div>
+                          {checked && (
+                            <div className="flex-shrink-0 text-white">
+                              <CheckCircle className="h-6 w-6" />
+                            </div>
+                          )}
+                        </div>
+                      </>
+                    )}
+                  </RadioGroup.Option>
+
+                  <RadioGroup.Option
+                    value="month"
+                    className={({ active, checked }) => `${
+                      active
+                        ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-secondary'
+                        : ''
+                    } ${
+                      checked
+                        ? 'bg-secondary bg-opacity-75 text-white'
+                        : 'bg-white'
+                    }
+                    relative flex w-11/12 cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none
+                  `}
+                  >
+                    {({ active, checked }) => (
+                      <>
+                        <div className="flex w-full items-center justify-between">
+                          <div className="flex items-center">
+                            <div className="text-sm">
+                              <RadioGroup.Label
+                                as="p"
+                                className={`font-medium  ${
+                                  checked ? 'text-white' : 'text-gray-900'
+                                }`}
+                              >
+                                Mensalmente
+                              </RadioGroup.Label>
+                            </div>
+                          </div>
+                          {checked && (
+                            <div className="flex-shrink-0 text-white">
+                              <CheckCircle className="h-6 w-6" />
+                            </div>
+                          )}
+                        </div>
+                      </>
+                    )}
+                  </RadioGroup.Option>
+                </RadioGroup>
+              </div>
+            </div>
+          </BottomSheet>
           <div className="flex flex-col items-center justify-end gap-4">
             <button
               type="submit"
