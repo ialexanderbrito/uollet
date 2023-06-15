61ddf393d7f9c9c1ff6fed72b2bda116c1a379ac
ialexanderbrito <xanderzinho26@gmail.com>
2023-06-15 18:00:00 -0300

    refactor: new component select

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -3,34 +3,35 @@ import { Fragment } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
 import { CaretUpDown, Check } from '@phosphor-icons/react';
 
+interface DaysProps {
+  id: number;
+  name: string;
+}
+
 interface SelectProps {
-  selected: {
-    name: string;
-    icon: string;
-  };
-  setSelected: (value: any) => void;
-  options: {
-    name: string;
-    icon: string;
-  }[];
   className?: string;
+  placeholder?: string;
+  options: DaysProps[] | any[];
+  selected: DaysProps | any;
+  setSelected: (value: DaysProps | any) => void;
 }
 
 export function Select({
+  className,
+  placeholder = 'Selecione um dia',
+  options,
   selected,
   setSelected,
-  options,
-  className,
 }: SelectProps) {
   return (
-    <Listbox value={selected} onChange={setSelected}>
+    <Listbox value={selected.name} onChange={setSelected}>
       <div className="relative mt-1">
         <Listbox.Button
-          className={`focus-visible:ring-offset-orange-30 relative h-14 w-full cursor-default  rounded-lg bg-backgroundCard p-4 py-2 pl-3 pr-10 text-left text-title outline-none focus:outline-none focus-visible:border-primary focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 dark:bg-backgroundCardDark dark:text-titleDark sm:text-sm ${className}`}
+          className={`focus-visible:ring-offset-orange-30 relative h-14 w-full cursor-default rounded-lg bg-backgroundCard p-4 py-2 pl-3 pr-7 text-left text-title outline-none focus:outline-none focus-visible:border-primary focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 dark:bg-backgroundCardDark dark:text-titleDark sm:text-sm ${className}`}
         >
-          {selected.name === '' ? (
+          {placeholder && selected.name === '' ? (
             <>
-              <span className="block truncate">Selecione uma categoria</span>
+              <span className="block truncate text-text">{placeholder}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <CaretUpDown
                   className="h-5 w-5 text-gray-400"
@@ -40,10 +41,10 @@ export function Select({
             </>
           ) : (
             <>
-              <span className="block truncate">{selected.name}</span>
+              <span className="block truncate text-title">{selected.name}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <CaretUpDown
-                  className="h-5 w-5 text-gray-400"
+                  className="h-5 w-5 text-title"
                   aria-hidden="true"
                 />
               </span>
@@ -56,18 +57,33 @@ export function Select({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-backgroundCard py-1 text-base text-title shadow-lg ring-1 ring-black ring-opacity-5 scrollbar scrollbar-thin scrollbar-track-backgroundCard scrollbar-thumb-secondary focus:outline-none dark:bg-backgroundCardDark dark:text-titleDark dark:scrollbar-track-backgroundDark sm:text-sm">
-            {options.map((category, index) => (
+          <Listbox.Options className="absolute z-50 mt-1 max-h-60 w-full overflow-auto rounded-md bg-backgroundCard py-1 text-base text-title shadow-lg ring-1 ring-black ring-opacity-5 scrollbar scrollbar-thin scrollbar-track-backgroundCard scrollbar-thumb-secondary focus:outline-none dark:bg-backgroundCardDark dark:text-titleDark dark:scrollbar-track-backgroundDark sm:text-sm">
+            <Listbox.Option
+              key={0}
+              className={({ active }) =>
+                `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                  active
+                    ? 'bg-secondary text-title dark:bg-secondaryDark dark:text-titleDark'
+                    : 'text-text dark:text-titleDark'
+                }`
+              }
+              value=""
+              disabled
+            >
+              {placeholder}
+            </Listbox.Option>
+
+            {options.map((person, personIdx) => (
               <Listbox.Option
-                key={index}
+                key={personIdx}
                 className={({ active }) =>
                   `relative cursor-default select-none py-2 pl-10 pr-4 ${
                     active
                       ? 'bg-secondary text-title dark:bg-secondaryDark dark:text-titleDark'
-                      : 'text-text dark:text-titleDark'
+                      : 'text-title dark:text-titleDark'
                   }`
                 }
-                value={category}
+                value={person}
               >
                 {({ selected }) => (
                   <>
@@ -76,7 +92,7 @@ export function Select({
                         selected ? 'font-medium' : 'font-normal'
                       }`}
                     >
-                      {category.name}
+                      {person.name}
                     </span>
                     {selected ? (
                       <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
