a0229cb66c37983958c9370c370c018d9cb93299
ialexanderbrito <xanderzinho26@gmail.com>
2023-08-23 13:17:00 -0300

    feat: add category default

--- src/utils/category.ts
+++ src/utils/category.ts
@@ -8,93 +8,116 @@ export const category = [
   {
     icon: money,
     name: 'Dinheiro',
+    category: 'cash',
   },
   {
     icon: bank,
     name: 'Bradesco',
+    category: 'bank',
   },
   {
     icon: bank,
     name: 'Banco do Brasil',
+    category: 'bank',
   },
   {
     icon: bank,
     name: 'Santander',
+    category: 'bank',
   },
   {
     icon: bank,
     name: 'Itaú',
+    category: 'bank',
   },
   {
     icon: bank,
     name: 'Caixa',
+    category: 'bank',
   },
   {
     icon: bank,
     name: 'NuConta',
+    category: 'bank',
   },
   {
     icon: bank,
     name: 'C6Bank',
+    category: 'bank',
   },
   {
     icon: bank,
     name: 'Inter',
+    category: 'bank',
   },
   {
     icon: bank,
     name: 'BTG Pactual',
+    category: 'bank',
   },
   {
     icon: chartLineUp,
     name: 'NuInvest',
+    category: 'investments',
   },
   {
     icon: chartLineUp,
     name: 'C6 Investimentos',
+    category: 'investments',
   },
   {
     icon: chartLineUp,
     name: 'Inter Investimentos',
+    category: 'investments',
   },
   {
     icon: chartLineUp,
     name: 'XP Investimentos',
+    category: 'investments',
   },
   {
     icon: chartLineUp,
     name: 'Clear',
+    category: 'investments',
   },
   {
     icon: chartLineUp,
     name: 'BTG Investimentos',
+    category: 'investments',
   },
   {
     icon: chartLineUp,
     name: 'Rico',
+    category: 'investments',
   },
   {
     icon: chartLineUp,
     name: 'Ágora Investimentos',
+    category: 'investments',
   },
   {
     icon: chartLineUp,
     name: 'Órama Investimentos',
+    category: 'investments',
   },
   {
     icon: btc,
     name: 'Binance',
+    category: 'crypto',
   },
   {
     icon: btc,
     name: 'Mercado Bitcoin',
+    category: 'crypto',
   },
   {
     icon: btc,
     name: 'Foxbit',
+    category: 'crypto',
   },
   {
     icon: piggyBank,
     name: 'Rendimentos',
+    category: 'savings',
   },
 ];
