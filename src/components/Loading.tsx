be9eaab166a7884ab011e760d7e394e6478db580
ialexanderbrito <xanderzinho26@gmail.com>
2023-08-23 13:37:25 -0300

    style: remove cursor pointer

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ import { Jelly } from '@uiball/loaders';
 
 export function Loading() {
   return (
-    <div className="z-1001 fixed bottom-0 left-0 right-0 top-0 flex h-full w-full cursor-pointer flex-col items-center justify-center bg-background dark:bg-backgroundDark">
+    <div className="z-1001 fixed bottom-0 left-0 right-0 top-0 flex h-full w-full flex-col items-center justify-center bg-background dark:bg-backgroundDark">
       <Jelly color="#5636d3" />
     </div>
   );
