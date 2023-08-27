788d547b8aaa7c2098579c5925c4d47b996e5367
ialexanderbrito <xanderzinho26@gmail.com>
2023-08-27 13:25:44 -0300

    feat: add total value and verify account

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,10 +3,12 @@ import { useNavigate, Link } from 'react-router-dom';
 import { RWebShare } from 'react-web-share';
 
 import {
+  CreditCard,
   Info,
   Key,
   LockKey,
   Question,
+  SealCheck,
   SignOut,
   Sun,
   UserMinus,
@@ -14,6 +16,8 @@ import {
 import connectionImg from 'assets/connection.svg';
 import creditCardImg from 'assets/credit_card.svg';
 import defaultAvatar from 'assets/default_user_avatar.png';
+import incomeImg from 'assets/income.svg';
+import outcomeImg from 'assets/outcome.svg';
 import savingsImg from 'assets/savings.svg';
 import welcomeImg from 'assets/welcome.svg';
 
@@ -26,9 +30,14 @@ import { About } from 'components/Modal/About';
 import { useModal } from 'components/Modal/useModal';
 import { Submenu } from 'components/Submenu';
 
+import { cn } from 'utils/cn';
+import { formatCurrency } from 'utils/formatCurrency';
+import { verifyLoginLastSevenDays } from 'utils/verifyLoginLastSevenDays';
+
 import { useAuth } from 'contexts/Auth';
 import { useTheme } from 'contexts/Theme';
 
+import { useInvestiments } from 'hooks/useInvestiments';
 import { useProfile } from 'hooks/useProfile';
 import { useTransactions } from 'hooks/useTransactions';
 
@@ -38,8 +47,18 @@ export function Profile() {
   const { user, logOut } = useAuth();
   const { deleteUser } = useProfile();
   const { copyToClipboard } = useModal();
-  const { loading, handleCloseModal, openModal, handleOpenModal, setLoading } =
-    useTransactions();
+  const {
+    loading,
+    handleCloseModal,
+    openModal,
+    handleOpenModal,
+    allTotal,
+    getTransactionsValuesTotal,
+  } = useTransactions();
+  const {
+    allTotal: allTotalInvestiments,
+    getTransactionsValuesTotal: getTransactionsValuesTotalInvestiments,
+  } = useInvestiments();
 
   const [openModalSuport, setOpenModalSuport] = useState(false);
   const [openModalAbout, setOpenModalAbout] = useState(false);
@@ -53,10 +72,8 @@ export function Profile() {
   }
 
   useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
+    getTransactionsValuesTotal();
+    getTransactionsValuesTotalInvestiments();
   }, []);
 
   return (
@@ -67,7 +84,7 @@ export function Profile() {
         <div className="flex w-full flex-col items-center justify-center bg-background dark:bg-backgroundDark">
           <Header title="Minha conta" />
 
-          <div className="flex h-screen w-full flex-col gap-4 p-4">
+          <div className="flex w-full flex-col gap-4 p-4">
             <Link
               to={`/profile/${user?.id}`}
               className="flex h-16 cursor-pointer flex-row items-center justify-start gap-4 rounded-lg bg-backgroundCard dark:bg-backgroundCardDark"
@@ -79,26 +96,61 @@ export function Profile() {
               />
 
               <div>
-                <span className="font-medium text-title dark:text-titleDark">
+                <span className="flex items-center font-medium text-title dark:text-titleDark">
                   {user?.user_metadata.full_name}
+
+                  {verifyLoginLastSevenDays(
+                    user?.updated_at,
+                    user?.user_metadata.phone,
+                  ) && (
+                    <span className="ml-1 text-xs font-normal text-blue-500 dark:text-blue-400">
+                      <SealCheck size={16} weight="fill" />
+                    </span>
+                  )}
                 </span>
                 <p className="text-sm font-normal text-text dark:text-textDark">
                   Perfil
                 </p>
               </div>
             </Link>
+            <div className="flex h-16 flex-row items-center justify-start gap-4 rounded-lg bg-backgroundCard dark:bg-backgroundCardDark">
+              <img
+                src={
+                  allTotal + allTotalInvestiments < 0 ? outcomeImg : incomeImg
+                }
+                alt={
+                  allTotal + allTotalInvestiments < 0 ? 'Saidas' : 'Entradas'
+                }
+                className="ml-3 h-8 w-8 rounded-lg object-cover"
+              />
+
+              <div>
+                <p className="text-sm font-normal text-text dark:text-textDark">
+                  Saldo total na carteira
+                </p>
+                <span
+                  className={cn(
+                    'font-medium ',
+                    allTotal + allTotalInvestiments < 0 && 'text-danger',
+                    allTotal + allTotalInvestiments > 0 && 'text-success',
+                  )}
+                >
+                  {formatCurrency(allTotal + allTotalInvestiments)}
+                </span>
+              </div>
+            </div>
             <div className="flex w-full min-w-full snap-x gap-4 overflow-x-scroll p-1 scrollbar-hide md:flex-row md:justify-center md:gap-4 md:overflow-x-auto md:overflow-y-hidden md:pb-4">
               <Banner
                 title="Seja bem vindo(a) ao Finances!"
-                color="#1da1f3"
                 img={welcomeImg}
+                className="bg-[#1da1f3]"
               />
 
               <Banner
                 title="Comece a controlar suas finanças agora mesmo!"
-                color="#01eefe"
                 img={savingsImg}
                 onClick={() => navigate('/register')}
+                className="bg-[#01eefe]"
               />
 
               <RWebShare
@@ -110,19 +162,27 @@ export function Profile() {
               >
                 <Banner
                   title=" Convide seus amigos para usar o Finances!"
-                  color="#fbbb02"
                   img={connectionImg}
+                  className="bg-[#fbbb02]"
                 />
               </RWebShare>
 
               <Banner
                 title="Adicione seus cartões de crédito!"
-                color="#01e59a"
                 img={creditCardImg}
                 onClick={() => navigate('/credit-card')}
+                className="bg-[#01e59a]"
               />
             </div>
             <div className="mt-4 flex flex-col items-start justify-center gap-4 text-title dark:text-titleDark">
+              <Submenu
+                icon={<CreditCard size={20} weight="light" />}
+                title="Cartões de crédito"
+                onClick={() => navigate('/cards')}
+                arrow
+                divider
+              />
+
               <Submenu
                 icon={<LockKey size={20} weight="light" />}
                 title="Segurança"
