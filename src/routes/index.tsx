cbda980191bc0e6a72cf63de8a51a77c6e8b1089
ialexanderbrito <xanderzinho26@gmail.com>
2023-09-03 00:48:25 -0300

    feat: add subdomain config

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,17 @@ import { PublicRoutes } from './PublicRoutes';
 
 export function MainRoutes() {
   const { signed } = useAuth();
+
+  if (window.location.host.split('.')[0] === 'app') {
+    return (
+      <>
+        <KBar>
+          <PrivateRoutes />
+        </KBar>
+      </>
+    );
+  }
+
   return signed ? (
     <>
       <KBar>
