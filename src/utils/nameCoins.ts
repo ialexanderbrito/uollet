685617c8f39434f3b4ea64c2ec3640d32b904f08
ialexanderbrito <xanderzinho26@gmail.com>
2023-09-10 19:54:12 -0300

    feat: add utils coins database

---
+++ src/utils/nameCoins.ts
@@ -0,0 +1,396 @@
+export const nameCoins = {
+  principais: [
+    {
+      code: 'BRL',
+      name: 'BRL - Real (R$)',
+      'data-unit': 'R$',
+    },
+    {
+      code: 'USD',
+      name: 'USD - Dólar Americano ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'EUR',
+      name: 'EUR - Euro (€)',
+      'data-unit': '€',
+    },
+    {
+      code: 'GBP',
+      name: 'GBP - Libra Esterlina (£)',
+      'data-unit': '£',
+    },
+    {
+      code: 'JPY',
+      name: 'JPY - Iene (¥)',
+      'data-unit': '¥',
+    },
+  ],
+  todas: [
+    {
+      code: 'ANG',
+      name: 'ANG - Florim (ƒ)',
+      'data-unit': 'ƒ',
+    },
+    {
+      code: 'ARS',
+      name: 'ARS - Peso Argentino ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'AUD',
+      name: 'AUD - Dólar australiano ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'BGN',
+      name: 'BGN - Lev (лв)',
+      'data-unit': 'лв',
+    },
+    {
+      code: 'BND',
+      name: 'BND - Dólar do Brunei ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'BOB',
+      name: 'BOB - Boliviano ($b)',
+      'data-unit': '$b',
+    },
+    {
+      code: 'BRL',
+      name: 'BRL - Real (R$)',
+      'data-unit': 'R$',
+    },
+    {
+      code: 'BWP',
+      name: 'BWP - Pula (P)',
+      'data-unit': 'P',
+    },
+    {
+      code: 'CAD',
+      name: 'CAD - Dólar canadense ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'CHF',
+      name: 'CHF - Franco suíço (CHF)',
+      'data-unit': 'CHF',
+    },
+    {
+      code: 'CLP',
+      name: 'CLP - Peso chileno ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'CNY',
+      name: 'CNY - Renminbi (¥)',
+      'data-unit': '¥',
+    },
+    {
+      code: 'COP',
+      name: 'COP - Peso colombiano ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'CRC',
+      name: 'CRC - Colon da Costa Rica (₡)',
+      'data-unit': '₡',
+    },
+    {
+      code: 'CZK',
+      name: 'CZK - Coroa (Kč)',
+      'data-unit': 'Kč',
+    },
+    {
+      code: 'DKK',
+      name: 'DKK - Coroa dinamarquesa (kr)',
+      'data-unit': 'kr',
+    },
+    {
+      code: 'DOP',
+      name: 'DOP - Peso (RD$)',
+      'data-unit': 'RD$',
+    },
+    {
+      code: 'EGP',
+      name: 'EGP - Libra egípcia (£)',
+      'data-unit': '£',
+    },
+    {
+      code: 'EUR',
+      name: 'EUR - Euro (€)',
+      'data-unit': '€',
+    },
+    {
+      code: 'FJD',
+      name: 'FJD - Dólar das Fiji ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'GBP',
+      name: 'GBP - Libra Esterlina (£)',
+      'data-unit': '£',
+    },
+    {
+      code: 'HKD',
+      name: 'HKD - Dólar de Hong Kong ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'HNL',
+      name: 'HNL - Lempira (L)',
+      'data-unit': 'L',
+    },
+    {
+      code: 'HRK',
+      name: 'HRK - Kuna (kn)',
+      'data-unit': 'kn',
+    },
+    {
+      code: 'HUF',
+      name: 'HUF - Forint (Ft)',
+      'data-unit': 'Ft',
+    },
+    {
+      code: 'IDR',
+      name: 'IDR - Rupia indonésia (Rp)',
+      'data-unit': 'Rp',
+    },
+    {
+      code: 'ILS',
+      name: 'ILS - Shekel (₪)',
+      'data-unit': '₪',
+    },
+    {
+      code: 'INR',
+      name: 'INR - Rupia indiana (₹)',
+      'data-unit': '₹',
+    },
+    {
+      code: 'JMD',
+      name: 'JMD - Dólar jamaicano (J$)',
+      'data-unit': 'J$',
+    },
+    {
+      code: 'JPY',
+      name: 'JPY - Iene (¥)',
+      'data-unit': '¥',
+    },
+    {
+      code: 'KRW',
+      name: 'KRW - Won sul coreano (₩)',
+      'data-unit': '₩',
+    },
+    {
+      code: 'KYD',
+      name: 'KYD - Dólar das Ilhas Caimão ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'KZT',
+      name: 'KZT - Tenge (лв)',
+      'data-unit': 'лв',
+    },
+    {
+      code: 'LBP',
+      name: 'LBP - Libra libanesa (£)',
+      'data-unit': '£',
+    },
+    {
+      code: 'LKR',
+      name: 'LKR - Rupia do Sri Lanka (₨)',
+      'data-unit': '₨',
+    },
+    {
+      code: 'LTL',
+      name: 'LTL - Litas (Lt)',
+      'data-unit': 'Lt',
+    },
+    {
+      code: 'LVL',
+      name: 'LVL - Lats (Ls)',
+      'data-unit': 'Ls',
+    },
+    {
+      code: 'MKD',
+      name: 'MKD - Denar (ден)',
+      'data-unit': 'ден',
+    },
+    {
+      code: 'MUR',
+      name: 'MUR - Rupia da Maurícia (₨)',
+      'data-unit': '₨',
+    },
+    {
+      code: 'MXN',
+      name: 'MXN - Peso Mexicano ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'MYR',
+      name: 'MYR - Ringgit (RM)',
+      'data-unit': 'RM',
+    },
+    {
+      code: 'NAD',
+      name: 'NAD - Dólar da Namíbia ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'NGN',
+      name: 'NGN - Naira (₦)',
+      'data-unit': '₦',
+    },
+    {
+      code: 'NIO',
+      name: 'NIO - Cordoba Oro (C$)',
+      'data-unit': 'C$',
+    },
+    {
+      code: 'NOK',
+      name: 'NOK - Coroa norueguesa (kr)',
+      'data-unit': 'kr',
+    },
+    {
+      code: 'NPR',
+      name: 'NPR - Rupia nepalesa (₨)',
+      'data-unit': '₨',
+    },
+    {
+      code: 'NZD',
+      name: 'NZD - Dólar da Nova Zelândia ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'OMR',
+      name: 'OMR - Rial Omani (﷼)',
+      'data-unit': '﷼',
+    },
+    {
+      code: 'PEN',
+      name: 'PEN - Nuevo Sol (S/.)',
+      'data-unit': 'S/.',
+    },
+    {
+      code: 'PHP',
+      name: 'PHP - Peso filipino (Php)',
+      'data-unit': 'Php',
+    },
+    {
+      code: 'PKR',
+      name: 'PKR - Rupia paquistanesa (₨)',
+      'data-unit': '₨',
+    },
+    {
+      code: 'PLN',
+      name: 'PLN - Zloty (zł)',
+      'data-unit': 'zł',
+    },
+    {
+      code: 'PYG',
+      name: 'PYG - Guarani (Gs)',
+      'data-unit': 'Gs',
+    },
+    {
+      code: 'QAR',
+      name: 'QAR - Rial do Qatar (﷼)',
+      'data-unit': '﷼',
+    },
+    {
+      code: 'RON',
+      name: 'RON - RON (lei)',
+      'data-unit': 'lei',
+    },
+    {
+      code: 'RSD',
+      name: 'RSD - Dinar Sérvio (Дин.)',
+      'data-unit': 'Дин.',
+    },
+    {
+      code: 'RUB',
+      name: 'RUB - Rublo (руб)',
+      'data-unit': 'руб',
+    },
+    {
+      code: 'SAR',
+      name: 'SAR - Riyal (﷼)',
+      'data-unit': '﷼',
+    },
+    {
+      code: 'SCR',
+      name: 'SCR - Rupia das Seychelles (₨)',
+      'data-unit': '₨',
+    },
+    {
+      code: 'SEK',
+      name: 'SEK - Coroa Sueca (kr)',
+      'data-unit': 'kr',
+    },
+    {
+      code: 'SGD',
+      name: 'SGD - Dólar de Cingapura ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'SVC',
+      name: 'SVC - Colon de El Salvador ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'THB',
+      name: 'THB - Baht (฿)',
+      'data-unit': '฿',
+    },
+    {
+      code: 'TRY',
+      name: 'TRY - Nova Lira turca (TL)',
+      'data-unit': 'TL',
+    },
+    {
+      code: 'TTD',
+      name: 'TTD - Dólar de Trindade e Tobago (TT$)',
+      'data-unit': 'TT$',
+    },
+    {
+      code: 'TWD',
+      name: 'TWD - Novo Dólar de Taiwan (NT$)',
+      'data-unit': 'NT$',
+    },
+    {
+      code: 'UAH',
+      name: 'UAH - Hryvnia (₴)',
+      'data-unit': '₴',
+    },
+    {
+      code: 'USD',
+      name: 'USD - Dólar Americano ($)',
+      'data-unit': '$',
+    },
+    {
+      code: 'UYU',
+      name: 'UYU - Peso Uruguaio ($U)',
+      'data-unit': '$U',
+    },
+    {
+      code: 'UZS',
+      name: 'UZS - Som Uzbeque (лв)',
+      'data-unit': 'лв',
+    },
+    {
+      code: 'VND',
+      name: 'VND - Dong (₫)',
+      'data-unit': '₫',
+    },
+    {
+      code: 'YER',
+      name: 'YER - Rial do Iémene (﷼)',
+      'data-unit': '﷼',
+    },
+    {
+      code: 'ZAR',
+      name: 'ZAR - Rand (R)',
+      'data-unit': 'R',
+    },
+  ],
+};
