4a75ab3d66e6236aaad54be010394f6af3036308
ialexanderbrito <xanderzinho26@gmail.com>
2023-09-10 18:56:08 -0300

    feat: add component input error

---
+++ src/components/InputError.tsx
@@ -0,0 +1,24 @@
+import { XCircle } from '@phosphor-icons/react';
+
+import { cn } from 'utils/cn';
+
+interface InputErrorProps {
+  error: boolean;
+  message: string;
+  className?: string;
+}
+
+export function InputError({ error, message, className }: InputErrorProps) {
+  return (
+    <div
+      className={cn('mt-[-0.5rem] flex w-full flex-col gap-4 pl-2', className)}
+    >
+      {error && (
+        <div className="flex items-center gap-1 text-sm text-danger">
+          <XCircle className="text-danger" />
+          <span className="text-xs text-danger">{message}</span>
+        </div>
+      )}
+    </div>
+  );
+}
