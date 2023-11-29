da131f7742d871a3b3c93ae07461f6cbd258f9da
ialexanderbrito <xanderzinho26@gmail.com>
2023-11-29 00:13:23 -0300

    feat: add new component card stock

---
+++ src/components/CardStock.tsx
@@ -0,0 +1,72 @@
+import { Star } from '@phosphor-icons/react';
+
+import { cn } from 'utils/cn';
+import { formatCurrency } from 'utils/formatCurrency';
+
+interface CardStockProps {
+  stock: string;
+  name: string;
+  close: number;
+  currency?: string;
+  change: number;
+  logo: string;
+  saveFavoriteStock: (stock: string) => void;
+  verifyIsFavorite: (stock: string) => boolean;
+  onClick?: () => void;
+}
+
+export function CardStock({
+  stock,
+  name,
+  close,
+  currency,
+  change,
+  logo,
+  saveFavoriteStock,
+  verifyIsFavorite,
+  onClick,
+}: CardStockProps) {
+  const isFavorite = verifyIsFavorite(stock);
+  const formattedClose = formatCurrency(close, currency);
+  const changeIndicator = change > 0 ? '▲' : '▼';
+  const changeColor = change > 0 ? 'text-success' : 'text-danger';
+
+  return (
+    <div
+      className="flex min-w-[300px] flex-grow cursor-pointer flex-col justify-center rounded-md bg-backgroundCard px-4 py-4 text-sm font-medium tracking-[-0.5px] transition-all dark:bg-backgroundCardDark"
+      onClick={onClick}
+    >
+      <div className="flex justify-between">
+        <span className="text-xl font-bold text-title dark:text-textDark">
+          {stock}
+        </span>
+        <img
+          src={logo}
+          alt={stock}
+          className="h-8 w-8 rounded-lg"
+          width={32}
+          height={32}
+          loading="lazy"
+        />
+      </div>
+      <span className="capitalize text-title dark:text-textDark">{name}</span>
+      <div className="flex justify-between">
+        <p className="text-title dark:text-textDark">{formattedClose}</p>
+        <div className="flex gap-2">
+          <p className={cn(changeColor)}>
+            {changeIndicator}
+            {change?.toFixed(2)}%
+          </p>
+          <p
+            className="flex w-6 cursor-pointer items-center justify-center rounded-md bg-primaryDark text-center text-white dark:text-textDark"
+            onClick={() => {
+              saveFavoriteStock(stock);
+            }}
+          >
+            {isFavorite ? <Star weight="fill" /> : <Star />}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
