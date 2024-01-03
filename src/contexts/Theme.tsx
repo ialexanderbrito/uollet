8271403e63c4934f6b1c540c4cae5fc64501fd07
ialexanderbrito <xanderzinho26@gmail.com>
2024-01-03 14:05:38 -0300

    refactor: context theme light, dark or auto

--- src/contexts/Theme.tsx
+++ src/contexts/Theme.tsx
@@ -1,34 +1,87 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 
 interface ThemeContextProps {
-  theme: string;
+  theme: string | null;
   toggleTheme: () => void;
   removeDarkTheme: () => void;
+  handleChangeColorMode: (mode: string) => void;
 }
 
 const ThemeContext = createContext<ThemeContextProps>({} as ThemeContextProps);
 
 export const ThemeProvider = ({ children }: any) => {
-  const themeStorage = localStorage.getItem('@uollet:theme');
-  const [theme, setTheme] = useState(themeStorage || 'light');
+  const [theme, setTheme] = useState(
+    localStorage.getItem('@uollet:theme')
+      ? localStorage.getItem('@uollet:theme')
+      : 'auto',
+  );
+
+  function handleChangeColorMode(mode: string) {
+    setTheme(mode);
+    localStorage.setItem('@uollet:theme', mode);
+  }
+
+  const colorScheme = document.documentElement;
 
   useEffect(() => {
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
+    const cachedTheme = localStorage.getItem('@uollet:theme') || 'auto';
+    const preferredTheme = window.matchMedia('(prefers-color-scheme: dark)')
+      .matches
+      ? 'dark'
+      : 'light';
+
+    function onWindowMatchMediaChange(e: MediaQueryListEvent) {
+      if (e.matches) {
+        colorScheme.classList.add('dark');
+        setTheme('auto');
+      } else {
+        colorScheme.classList.remove('dark');
+        setTheme('auto');
+      }
     }
+
+    switch (cachedTheme) {
+      case 'dark':
+        colorScheme.classList.add('dark');
+        setTheme('dark');
+        break;
+      case 'light':
+        colorScheme.classList.remove('dark');
+        setTheme('light');
+        break;
+      case 'auto':
+        if (preferredTheme === 'dark') {
+          colorScheme.classList.add('dark');
+          setTheme('auto');
+        } else {
+          colorScheme.classList.remove('dark');
+          setTheme('auto');
+        }
+        break;
+      default:
+        break;
+    }
+
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', onWindowMatchMediaChange);
+
+    return () => {
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .removeEventListener('change', onWindowMatchMediaChange);
+    };
   }, [theme]);
 
   function toggleTheme() {
-    setTheme(theme === 'dark' ? 'light' : 'dark');
-    localStorage.setItem('@uollet:theme', theme === 'dark' ? 'light' : 'dark');
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    setTheme(newTheme);
+    localStorage.setItem('@uollet:theme', newTheme);
   }
 
   function removeDarkTheme() {
     setTheme('light');
     localStorage.setItem('@uollet:theme', 'light');
-    document.documentElement.classList.remove('dark');
   }
 
   return (
@@ -37,6 +90,7 @@ export const ThemeProvider = ({ children }: any) => {
         theme,
         toggleTheme,
         removeDarkTheme,
+        handleChangeColorMode,
       }}
     >
       {children}
