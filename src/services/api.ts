048eec8fa288bc948e52f9095d01e7505f639265
ialexanderbrito <xanderzinho26@gmail.com>
2024-01-27 15:46:09 -0300

    feat: add axios interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,20 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://api.uollet.com.br/',
+  baseURL: 'https://api.uollet.com.br',
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = sessionStorage.getItem('@uollet:token');
+  const refreshToken = sessionStorage.getItem('@uollet:refreshToken');
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  if (refreshToken) {
+    config.headers.refresh_token = refreshToken;
+  }
+
+  return config;
 });
