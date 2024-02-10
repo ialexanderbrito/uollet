26f9bf72d2dbb99db610b0566c6032e878670106
ialexanderbrito <xanderzinho26@gmail.com>
2024-02-10 13:50:11 -0300

    feat: add env url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,22 @@
 import axios from 'axios';
 
+console.log(import.meta.env.VITE_URL_API);
+
 export const api = axios.create({
-  baseURL: 'https://api.uollet.com.br/',
+  baseURL: import.meta.env.VITE_URL_API,
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = sessionStorage.getItem('@uollet:token');
+  const refreshToken = sessionStorage.getItem('@uollet:refreshToken');
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  if (refreshToken) {
+    config.headers.refresh_token = refreshToken;
+  }
+
+  return config;
 });
