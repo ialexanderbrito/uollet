46985e564065b8c501f055e74e6442e449714ffa
ialexanderbrito <xanderzinho26@gmail.com>
2024-02-24 19:56:08 -0300

    fix: microsoft clarity env

--- src/config/MicrosoftClarity.tsx
+++ src/config/MicrosoftClarity.tsx
@@ -6,7 +6,7 @@ export function MicrosoftClarity() {
     script.id = 'microsoft-clarity-init';
     script.type = 'text/javascript';
     script.async = true;
-    script.src = `https://www.clarity.ms/tag/${process.env.VITE_MICROSOFT_CLARITY_ID}`;
+    script.src = `https://www.clarity.ms/tag/${import.meta.env.VITE_MICROSOFT_CLARITY_ID}`;
 
     const firstScript = document.getElementsByTagName('script')[0];
 
@@ -14,7 +14,6 @@ export function MicrosoftClarity() {
       firstScript.parentNode.insertBefore(script, firstScript);
 
       return () => {
-        // Limpeza se necessário, por exemplo, se você desmontar o componente
         const clarityScript = document.getElementById('microsoft-clarity-init');
         clarityScript?.parentNode?.removeChild(clarityScript);
       };
