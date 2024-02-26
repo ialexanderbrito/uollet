42ec45bc2da1a3886b3493b0fc90453bab8de532
ialexanderbrito <xanderzinho26@gmail.com>
2024-02-25 23:14:02 -0300

    feat: add props page error

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -4,24 +4,37 @@ import { errorIllustration } from 'assets/illustrations';
 
 import { Button } from 'components/Button';
 
-export function Error() {
+interface ErrorProps {
+  title?: string;
+  description?: string;
+  buttonText?: string;
+  link?: string;
+}
+
+export function Error({
+  title = 'Infelizmente a página não foi encontrada!',
+  description,
+  buttonText = 'Voltar para página inicial',
+  link = '/',
+}: ErrorProps) {
   const navigate = useNavigate();
 
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center bg-background p-4 dark:bg-background-dark">
       <img src={errorIllustration} alt="Erro 404" className="h-96 w-96" />
       <h1 className="text-sm font-normal text-title dark:text-title-dark">
-        Infelizmente a página não foi encontrada!
+        {title}
       </h1>
+      <p className="text-body dark:text-body-dark text-xs">{description}</p>
 
       <div className="mt-10 flex w-full flex-col gap-4">
         <Button
           type="button"
           onClick={() => {
-            navigate('/');
+            navigate(link);
           }}
         >
-          Voltar para página inicial
+          {buttonText}
         </Button>
       </div>
     </div>
